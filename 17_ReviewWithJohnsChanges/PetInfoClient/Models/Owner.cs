11cc4816a9c66cb3732b6cb49c2f1b48d3389558
John Fulton <john@techelevator.com>
2021-03-02 12:42:17 -0500

    17_ReviewWithJohnsChanges/

---
+++ 17_ReviewWithJohnsChanges/PetInfoClient/Models/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetInfoClient.Models
+{
+    public class Owner
+    {
+        public int OwnerId { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{OwnerId} - {FullName} - {Phone} - {Email}";
+        }
+    }
+}
